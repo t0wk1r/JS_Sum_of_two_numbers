912672636ccb6706b3e0d4d0144b497a75ef557c
MD. ABU SAYED <64466834+t0wk1r@users.noreply.github.com>
2022-01-31 23:05:18 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function result() {
+    var a, b, c;
+    a = Number(document.getElementById("number1").value);
+    b = Number(document.getElementById("number2").value);
+
+    c = a + b;
+
+    //document.getElementById("demo").innerHTML = c;
+    console.log(c);
+}
